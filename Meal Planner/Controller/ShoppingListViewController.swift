2d232b73cd386098c864504303c386de4800b922
Haddawi <muhamadhaddawi@gmail.com>
2020-05-18 15:44:38 +0700

    feature: impl shopping list collection view with dummy data

---
+++ Meal Planner/Controller/ShoppingListViewController.swift
@@ -0,0 +1,51 @@
+//
+//  ShoppingListViewController.swift
+//  Meal Planner
+//
+//  Created by Haddawi on 18/05/20.
+//  Copyright © 2020 Team13. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class ShoppingListViewController: UITableViewController {
+    
+    @IBOutlet weak var ingredientsSearchController: UISearchBar!
+    
+    struct Ingredient {
+        var name: String
+        var quantity: String
+    }
+    
+    let Ingredients = [
+        Ingredient(name: "Ayam", quantity: "1 ekor utuh"),
+        Ingredient(name: "Sapi", quantity: "1/2 kg"),
+        Ingredient(name: "Ikan Bawal", quantity: "3 kg"),
+        Ingredient(name: "Laos Geprek", quantity: "3 cm"),
+        Ingredient(name: "Gula", quantity: "250 gram"),
+        Ingredient(name: "Daun Salam", quantity: "2 lembar"),
+        Ingredient(name: "Ceker Ayam", quantity: "1/2 kg")
+    ]
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return Ingredients.count
+    }
+
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        
+        let ingredientItem = Ingredients[indexPath.row]
+        
+        cell.textLabel?.text = ingredientItem.name
+        cell.detailTextLabel?.text = ingredientItem.quantity
+        cell.imageView?.image = UIImage(named: "checkbox-marked")
+        
+        return cell
+    }
+}
