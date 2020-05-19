7da14d693acb773afa3cdcf9d5cd522ee2d7b2fe
Haddawi <muhamadhaddawi@gmail.com>
2020-05-19 14:02:37 +0700

    feature: prepare for search ingredients item on shopping list

--- Meal Planner/Controller/ShoppingListViewController.swift
+++ Meal Planner/Controller/ShoppingListViewController.swift
@@ -11,14 +11,12 @@ import UIKit
 
 class ShoppingListViewController: UITableViewController {
     
-    @IBOutlet weak var ingredientsSearchController: UISearchBar!
-    
     struct Ingredient {
         var name: String
         var quantity: String
     }
     
-    let Ingredients = [
+    let ingredients = [
         Ingredient(name: "Ayam", quantity: "1 ekor utuh"),
         Ingredient(name: "Sapi", quantity: "1/2 kg"),
         Ingredient(name: "Ikan Bawal", quantity: "3 kg"),
@@ -27,25 +25,29 @@ class ShoppingListViewController: UITableViewController {
         Ingredient(name: "Daun Salam", quantity: "2 lembar"),
         Ingredient(name: "Ceker Ayam", quantity: "1/2 kg")
     ]
+    var currentIngredients: [Ingredient] = []
     
     override func viewDidLoad() {
         super.viewDidLoad()
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return Ingredients.count
+        return ingredients.count
     }
 
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         
         let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
         
-        let ingredientItem = Ingredients[indexPath.row]
+        let ingredientItem = ingredients[indexPath.row]
         
         cell.textLabel?.text = ingredientItem.name
         cell.detailTextLabel?.text = ingredientItem.quantity
         cell.imageView?.image = UIImage(named: "checkbox-marked")
         
         return cell
+    }
+    
+    func filterCurrentDataSource(searchKey: String) {
     }
 }
