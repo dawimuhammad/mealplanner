746469e96676714c2e0f8f72a214fc2b322711f3
Haddawi <muhamadhaddawi@gmail.com>
2020-05-26 14:57:13 +0700

    feat/shopping-list-core: removed unused variables & struct data models

--- Meal Planner/Controller/ShoppingListViewController.swift
+++ Meal Planner/Controller/ShoppingListViewController.swift
@@ -12,7 +12,6 @@ import UIKit
 class ShoppingListViewController: UITableViewController {
     
     var shoppingLists: [ShoppingList] = []
-    var shoppingListsWithSection: [ShoppingListItemSection] = []
     
     override func viewDidLoad() {
         super.viewDidLoad()
